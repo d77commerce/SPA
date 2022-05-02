806d39d1e350efe455ed0e32e34f276772c9e813
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-05-02 21:29:34 +0100

    new

--- SPA page-router/view/contactsView.js
+++ SPA page-router/view/contactsView.js
@@ -7,9 +7,16 @@ const contactsEl = () => html`<h1>CONTACTS</h1>
     <input type="text" id="fname" name="fname" /><br />
     <label for="lname">Last name:</label><br />
     <input type="text" id="lname" name="lname" /><br />
+    <label type="text">Email</label><br />
+    <input type="email" id="email" /><br />
     <label type="text">Your message</label><br />
-    <textarea rows="4" cols="50" name="comment" form="usrform">
-Enter text here...</textarea
+    <textarea
+      rows="4"
+      cols="50"
+      name="comment"
+      form="usrform"
+      placeholder="Enter text here..."
+    ></textarea
     ><br /><br />
 
     <input type="submit" value="Submit" />
